c8b6f4273b0e1fc77c60db50f214ba3949168b6b
Andrei_Bondarenko <bondorol007@gmail.com>
2022-09-01 14:56:22 +0300

    headFirstJava3

---
+++ src/GoodDog.java
@@ -0,0 +1,18 @@
+public class GoodDog {
+    private int size;
+    public int getSize(){
+        return size;
+    }
+    public void setSize(int s){
+        size = s;
+    }
+    void bark(){
+        if(size > 60){
+            System.out.println("WOOOF - WOOOF");
+        } else if (size > 14){
+            System.out.println("woof - woof");
+        } else {
+            System.out.println("tyaf - tyaf");
+        }
+    }
+}
